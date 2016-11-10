8adb776d301bcccc575c0760d1bf7e3a56d73a9f
Rung921 <richeung921@hotmail.com>
2016-11-10 11:37:00 -0800

    GameBoard.hpp v1

--- GameBoard.hpp
+++ GameBoard.hpp
@@ -1,8 +1,13 @@
+// GameBoard.hpp
+// CPSC 131, Fall 2016, Project 2
 //
-//  GameBoard.hpp
+// Class that constructs the gameboard for chutes and ladders
 //
+// Name: Richard Ung
+// Due:  November 11, 2016
 
-#pragma once
+#ifndef GAMEBOARD_HPP
+#define GAMEBOARD_HPP
 
 #include <stdexcept>
 
@@ -12,33 +17,28 @@ const int OUTSIDE_BOARD = -1;
 const int BOARD_SIZE = 101;
 
 class GameBoard {
-public:   
-   enum Ladders { ONE = 1, FOUR = 4, NINE = 9, TWENTY_ONE = 21, TWENTY_EIGHT = 28, THIRTY_SIX = 36, FIFTY_ONE = 51, SEVENTY_ONE = 71, EIGHTY = 80 };
-   
-   enum Chutes { SIXTEEN = 16, FORTY_SEVEN = 47, FORTY_NINE = 49, FIFTY_SIX = 56, SIXTY_TWO = 62, SIXTY_FOUR = 64, EIGHTY_SEVEN = 87, NINETY_THREE = 93, NINETY_FIVE = 95, NINETY_EIGHT = 98 };
-   
-   // Build the gameboard
-   // TO DO: implement this function
-   void buildBoard();
-   
-   GameBoard() {
-      // TODO: implement this function properly
-      throw std::logic_error("not implemented yet");
-   }
-   
-   // If player lands on chutes or ladders, returns the new position.
-   // Otherwise, returns the player's current position.
-   // If the player's position is outside of the gameboard, throws index out of bounds exception
-   // TO DO: implement this function properly
-   int checkChutesLadders(int position) {
-      if ((position < 0) || (position >= BOARD_SIZE)) {
-         throw range_error("index out of bounds");
-      }
-      // TODO: implement this function properly
-      throw std::logic_error("not implemented yet");
-   }
-   
+public:
+	enum Ladders { ONE = 1, FOUR = 4, NINE = 9, TWENTY_ONE = 21, TWENTY_EIGHT = 28, THIRTY_SIX = 36, FIFTY_ONE = 51, SEVENTY_ONE = 71, EIGHTY = 80 };
+
+	enum Chutes { SIXTEEN = 16, FORTY_SEVEN = 47, FORTY_NINE = 49, FIFTY_SIX = 56, SIXTY_TWO = 62, SIXTY_FOUR = 64, EIGHTY_SEVEN = 87, NINETY_THREE = 93, NINETY_FIVE = 95, NINETY_EIGHT = 98 };
+
+	// Build the gameboard
+	void buildBoard();
+
+	GameBoard() { buildBoard(); }
+
+	// If player lands on chutes or ladders, returns the new position.
+	// Otherwise, returns the player's current position.
+	// If the player's position is outside of the gameboard, throws index out of bounds exception
+	int checkChutesLadders(int position) {
+		if ((position < 0) || (position >= BOARD_SIZE)) {
+			throw range_error("index out of bounds");
+		}
+		return _posList[position];
+	}
+
 private:
-   // TO DO: add storage for squares including square of chutes and ladders
-   // Requirement: use ExtendableVector to store the square
+	ExtendableVector<int> _posList;
 };
+
+#endif
