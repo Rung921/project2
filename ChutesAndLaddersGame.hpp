edef2ea07140541a784978af820505b5a1dd1de6
Rung921 <richeung921@hotmail.com>
2016-11-10 11:35:48 -0800

    ChutesAndLadders.hpp v1

--- ChutesAndLaddersGame.hpp
+++ ChutesAndLaddersGame.hpp
@@ -1,8 +1,13 @@
+// ChutesAndLaddersGame.hpp
+// CPSC 131, Fall 2016, Project 2
 //
-//  ChutesAndLaddersGame.hpp
+// Main class for the chutes and ladders game
 //
+// Name: Richard Ung
+// Due:  November 11, 2016
 
-#pragma once
+#ifndef CHUTESANDLADDERSGAME_HPP
+#define CHUTESANDLADDERSGAME_HPP
 
 #include <stdio.h>
 #include <string>
@@ -13,39 +18,34 @@
 
 using namespace std;
 
-// TO DO - change the default value to the number of your team members
-const int MIN_NUMBER_OF_PLAYERS = 2;
+// Default number of players - Number of team mates: 1
+const int MIN_NUMBER_OF_PLAYERS = 3;
 
 class ChutesAndLaddersGame {
 public:
-   // TODO: implement the constructor with all your team members
-   // constructor with the default value of 2 minimim players
-   ChutesAndLaddersGame(int nPlayers = MIN_NUMBER_OF_PLAYERS);
+	// constructor with the default value of 1 minimim player(s)
+	ChutesAndLaddersGame(int nPlayers = MIN_NUMBER_OF_PLAYERS);
 
-   // TODO: implement the destructor
-   // destructor
-   ~ChutesAndLaddersGame();
-   
-   // accessors
-   // TO DO: implement the accessor to get the number Of Players
-   long getNumberOfPlayers() {
-      // TODO: implement this function properly
-      throw std::logic_error("not implemented yet");
-   }
-   string getWinner() { return winner; }
-   
-   // TODO: implement this function properly
-   // reset the game - (1) this will reset the position of each player to zero
-   void resetGame();
-   
-   // TODO: implement this function properly
-   // play the Chutes and Ladders game
-   void playGame();
+	// destructor
+	~ChutesAndLaddersGame();
+
+	// accessors
+	long getNumberOfPlayers() { return _playerList.size(); }
+	string getWinner() { return _winner; }
+
+	// reset the game - (1) this will reset the position of each player to zero
+	void resetGame();
+
+	// play the Chutes and Ladders game
+	void playGame();
 private:
-   const int WINNING_POSITION = 100;
-   
-   string winner;       // the winner
-   GameBoard gameBoard; // the game board
+	const int WINNING_POSITION = 100;
 
-   // TO DO: use ArrayQueue to store the players
+	string _winner;       // the winner
+	GameBoard _gameBoard; // the game board
+
+	// TO DO: use ArrayQueue to store the players
+	ArrayQueue<Player*> _playerList;
 };
+
+#endif
